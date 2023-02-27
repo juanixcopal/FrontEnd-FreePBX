d55a9dd3f3d441c3f08d06ddede08a4b27fda954
Juan.Ixcopal <juan.ixcopal@alumnos.uneatlantico.es>
2023-02-27 18:16:41 +0100

    Feature: Button audio

--- src/pages/ftpRecording/index.js
+++ src/pages/ftpRecording/index.js
@@ -7,9 +7,22 @@ const PlayRecording = () => {
 
   console.log(ftpRecording)
 
+  const PlayRecording = name => {
+    const audio = new Audio(`../../var/spool/asterisk/monitor/2023/02/23/${name}`)
+    audio.play()
+    console.log('Audio', audio)
+  }
+
   return (
     <div>
-      <h2>Hola</h2>
+      <h2>Reproducir Grabacion</h2>
+      <ul>
+        {ftpRecording.map(item => {
+          const { type, name } = item
+
+          return <li key={name}>{type === '-' && name.endsWith('.wav') && <button onClick={() => PlayRecording(name)}>{name}</button>}</li>
+        })}
+      </ul>
     </div>
   )
 }
